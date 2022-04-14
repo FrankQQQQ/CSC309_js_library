507b12789e2ef150805eef271686f1a2d9d5b216
FrankQQQQ <18518553618@163.com>
2022-04-14 00:31:10 -0400

    finished

---
+++ Example.js
@@ -0,0 +1,15 @@
+
+
+
+
+my_calendar = new Calendarrr()
+
+
+let example_zero = my_calendar.showSpecificCalendar();
+document.getElementById("example_zero").appendChild(example_zero)
+
+let example_one = my_calendar.makeEditableFunctionalCalendar(2020, 5);
+document.getElementById("example_one").appendChild(example_one)
+
+let example_minus1 = my_calendar.functionalCalendar(2020, 5);
+document.getElementById("example_minus1").appendChild(example_minus1)
